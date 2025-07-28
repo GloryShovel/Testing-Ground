ba6cc8aa4e18375dc0f4929358794502f7d8daee
GloryShovel <sk.kamil6@gmail.com>
2025-07-28 21:46:55 +0200

    Engine Update

---
+++ Assets/SimpleFPS/Scripts/SimpleFPS.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleFPS : MonoBehaviour
+{
+    public Text display_Text;
+
+    int avgFrameRate, currentFrameRate;
+
+    public void Update()
+    {
+        avgFrameRate = (int)(Time.frameCount / Time.time);
+        currentFrameRate = (int)(1f / Time.unscaledDeltaTime);
+
+        display_Text.text = "Average: " + avgFrameRate.ToString() + " FPS  " + "Curent: " + currentFrameRate.ToString() + " FPS";
+    }
+}
